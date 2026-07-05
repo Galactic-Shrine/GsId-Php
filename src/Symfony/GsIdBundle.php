240a75335bcddf2a6674afee984d429788f38864
Damien James Ramon <damien.ramon@laposte.net>
2026-07-05 20:02:24 +0200

    Fix Symfony bundle extension alias

--- src/Symfony/GsIdBundle.php
+++ src/Symfony/GsIdBundle.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace GalacticShrine\GsId\Symfony;
 
+use GalacticShrine\GsId\Symfony\DependencyInjection\GsIdExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class GsIdBundle extends Bundle
 {
+    public function getContainerExtension(): ?ExtensionInterface
+    {
+        return new GsIdExtension();
+    }
 }
